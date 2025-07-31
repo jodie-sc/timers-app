78662229d2e34de0cf477e5a757e0bfd66f85138
Jodie <110611614+jodie-sc@users.noreply.github.com>
2025-07-31 18:08:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,18 @@ resetBtn.addEventListener("click", resetTimer);
 updateDisplay();
 
 
-if ('service {
-  navigator.serviceWorker.register('service-worker.js');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
 }
+
 
